7a129087de4353a25cb56c7405f71ebcf006f606
BigCubeCat <bigcubecat@yandex.ru>
2024-03-02 11:51:34 +0700

    feat(data): add simple script to collect results

---
+++ collect_data.py
@@ -0,0 +1,33 @@
+#!/bin/python
+import os
+import sys
+
+if len(sys.argv) < 4:
+    print("usage: collect_data.py <folder> <extension>")
+    sys.exit()
+
+folder_path = sys.argv[1]
+file_extension = sys.argv[2]
+res_path = sys.argv[3]
+
+files = os.listdir(folder_path)
+
+header = []
+data = []
+
+for file in files:
+    if file.endswith(file_extension):
+        with open(os.path.join(folder_path, file), 'r') as f:
+            content = f.readline()
+            try:
+                res = float(content)
+                header.append(file)
+                data.append(str(res))
+            except expression as identifier:
+                pass
+
+file_content = ';'.join(header) + '\n' + ';'.join(data)
+with open(res_path, "w", encoding="utf-8") as res_file: 
+    res_file.write(file_content)
+
+
