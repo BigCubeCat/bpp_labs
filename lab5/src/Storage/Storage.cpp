01e28e752833620b4a597b2159cb375b7eb715bb
BigCubeCat <bigcubecat@yandex.ru>
2024-04-29 00:53:32 +0700

    feat(Store): create storage

---
+++ lab5/src/Storage/Storage.cpp
@@ -0,0 +1,28 @@
+#include "Storage.h"
+
+Storage::Storage(int size) : maxSize(size) {
+}
+
+void Storage::addValue(const std::string &key, const std::string &value) {
+    queue.push(key);
+    map[key] = value;
+
+    if (queue.size() > maxSize) {
+        removeOldest();
+    }
+}
+
+void Storage::removeOldest() {
+    while (queue.size() > maxSize) {
+        auto k = queue.front();
+        queue.pop();
+        map.erase(k);
+    }
+}
+
+std::string Storage::getValue(const std::string &key) {
+    if (map.find(key) != map.end()) {
+        return map[key];
+    }
+    return "";
+}
---
+++ lab5/src/Storage/Storage.h
@@ -0,0 +1,26 @@
+#ifndef LAB5_STORAGE_H
+#define LAB5_STORAGE_H
+
+
+#include <map>
+#include <queue>
+#include <string>
+
+class Storage {
+public:
+    Storage(int size);
+
+    void addValue(const std::string &key, const std::string &value);
+    std::string getValue(const std::string &key);
+
+private:
+    int maxSize;
+    int size;
+    std::map<std::string, std::string> map{};
+    std::queue<std::string> queue{};
+
+    void removeOldest();
+};
+
+
+#endif //LAB5_STORAGE_H
